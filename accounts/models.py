87be487c3dc90b311c82fa5d448d4c22a713f07d
sjhmlj <sjhmlj@gmail.com>
2022-11-01 12:01:38 +0900

    회원가입, 로그인

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
 
 # Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(max_length=10)
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
+    profile_image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(120, 120)],  ## 유튜브 프로필 이미지 크기 120x120
+        format="JPEG",
+        options={"quality": 60},  ## 구글링한 프로필 이미지 퀄리티가 대체로 60으로 되어 있었음
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import get_object_or_404
+from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            messages.success(request, "Welcome!")
+            return redirect("reviews:index")
+    else:
+        form = CustomUserCreationForm
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("reviews:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("reviews:index")
+
+
+def detail(request, pk):
+    user = get_object_or_404(get_user_model(), pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+def profile_update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            return redirect("accounts:detail", request.user.pk)
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/update.html", context)
+
+
+def password_update(requets):
+    pass
+
+
+def delete(request):
+    try:
+        auth_logout(request, request.user)
+        request.user.delete()
+        messages.success(request, "The user is deleted")
+    except:
+        messages.error(request, "error")
+        return render(request, "reviews:index")
+    return redirect("reviews:index")
