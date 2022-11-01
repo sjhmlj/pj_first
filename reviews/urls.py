8ef12327c657caaf2a98fd561a617fb8d7b17fad
dongwonbaek <bmrio100@gmail.com>
2022-11-01 11:52:31 +0900

    reviews/configurations

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
+from . import views
+
+app_name = 'reviews'
 
 urlpatterns = [
-    path('',),
-    
+    path('', views.index, name='index'),
+    path('movie_create/', views.movie_create, name='movie_create'),
+    path('<int:pk>/', views.movie_detail, name='movie_detail'),
+    path('<int:pk>/movie_update/', views.movie_update, name='movie_update'),
+    path('<int:pk>/review_create/', views.review_create, name='review_create'),
+    path('<int:movie_pk>/review/<int:review_pk>/', views.review_detail, name='review_detail'),
+    path('<int:movie_pk>/review/<int:review_pk>/update/', views.review_update, name='review_update'),
+    path('<int:movie_pk>/review/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
+    path('<int:movie_pk>/review/<int:review_pk>/comment_create/', views.comment_create, name='comment_create'),
+    path('<int:movie_pk>/review/<int:review_pk>/comment_delete/<int:comment_pk>/', views.comment_delete, name='comment_delete'),
+    path('<int:movie_pk>/review/<int:review_pk>/like', views.like, name='like'),
+
 ]
