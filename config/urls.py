38bbc53ccea0d614e7fabbbfe0647af666e2ca3f
kyominkim1074 <kyominkim1074@gmail.com>
2022-11-02 11:31:44 +0900

    search page

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
@@ -8,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("reviews/", include("reviews.urls")),
+    path("search/", include("search.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- search/views.py
+++ search/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from accounts.models import User
+from reviews.models import Review, Movie
+from django.db.models import Q
 
 # Create your views here.
+
+
+def searchResult(request):
+    user = None
+    review = None
+    movie = None
+    query = None
+    if "q" in request.GET:
+        query = request.GET.get("q")
+        user = User.objects.all().filter(Q(username__contains=query))
+        review = Review.objects.all().filter(
+            Q(title__contains=query) | Q(content__contains=query)
+        )
+        movie = Review.objects.all().filter(
+            Q(title__contains=query) | Q(content__contains=query)
+        )
+    context = {
+        "query": query,
+        "user": user,
+        "review": review,
+        "movie": movie,
+    }
+    return render(request, "search/search.html", context)
